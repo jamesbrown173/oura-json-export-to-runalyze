8c302286f2beb63a4cd86578b9681d10f4110e8c
jamesbrown173 <jamesbrown173@gmail.com>
2024-04-24 22:11:07 +0700

    added basic UI functionality
    - added the basic UI
    - also added a function for first time users, if the user hasn't already entered a token it is entered then saved

--- main.py
+++ main.py
@@ -1,91 +1,126 @@
+
+
+
 import json
-import pprint
 import requests
+import tkinter as tk
+from tkinter import filedialog
 
 ########################################################################
-# Import the data and define the json to be submitted in the put request
+# The UI
 ########################################################################
 
-with open("./data/replace_this_with_your_data", "r") as file:
-    # Retrieve data from the file
-    contents = json.load(file)
+# Open the file and retrieve the values
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
+    if file_path:
+        with open("./data/my_data.json", "r") as file:
+            # Retrieve data from the file
+            contents = json.load(file)
 
-    #Access the 'Sleep' key and get the most recent entry
-    sleep_data = contents['sleep']
-    most_recent_entry = sleep_data[-1]
+            #Access the 'Sleep' key and get the most recent entry
+            sleep_data = contents['sleep']
+            most_recent_entry = sleep_data[-1]
 
-    # Define the values that we want to extract and create a dictionary for the SLEEP data
-    data_sleep = {
-        "start_time": most_recent_entry["bedtime_start"],
-        "duration": round(most_recent_entry["time_in_bed"] / 60),
-        "rem_duration": round(most_recent_entry["rem_sleep_duration"] / 60),
-        "light_sleep_duration": round(most_recent_entry["light_sleep_duration"] / 60),
-        "deep_sleep_duration": round(most_recent_entry["deep_sleep_duration"] / 60),
-        "awake_duration": round(most_recent_entry["awake_time"] / 60),
-        "hr_average": round(most_recent_entry["average_heart_rate"]),
-        "hr_lowest": round(most_recent_entry["lowest_heart_rate"]),
-        "quality": round(most_recent_entry["score"] / 10),
-    }
-    # Define the values that we want to extract and create a dictionary for the HRV data
-    # Here the HRV data uses the bedtime_end as its time, however it's a nightly average
-    data_hrv = {
-        "date_time": most_recent_entry["bedtime_end"],
-        "measurement_type": "asleep",
-        "hrv": 55,
-    }
+            # Define the values that we want to extract and create a dictionary for the SLEEP data
+            data_sleep = {
+                "start_time": most_recent_entry["bedtime_start"],
+                "duration": round(most_recent_entry["time_in_bed"] / 60),
+                "rem_duration": round(most_recent_entry["rem_sleep_duration"] / 60),
+                "light_sleep_duration": round(most_recent_entry["light_sleep_duration"] / 60),
+                "deep_sleep_duration": round(most_recent_entry["deep_sleep_duration"] / 60),
+                "awake_duration": round(most_recent_entry["awake_time"] / 60),
+                "hr_average": round(most_recent_entry["average_heart_rate"]),
+                "hr_lowest": round(most_recent_entry["lowest_heart_rate"]),
+                "quality": round(most_recent_entry["score"] / 10),
+            }
+            # Define the values that we want to extract and create a dictionary for the HRV data
+            # Here the HRV data uses the bedtime_end as its time, however it's a nightly average
+            data_hrv = {
+                "date_time": most_recent_entry["bedtime_end"],
+                "measurement_type": "asleep",
+                "hrv": round(most_recent_entry["average_hrv"]),
+            }
 
-    # sample_data_sleep = {
-    #   "start_time": "2024-04-30T22:52:33.000+07:00",
-    #   "duration": 370,
-    #   "rem_duration": 46,
-    #   "light_sleep_duration": 211,
-    #   "deep_sleep_duration": 118,
-    #   "awake_duration": 50,
-    #   "hr_average": 57,
-    #   "hr_lowest": 47,
-    #   "quality": 8
-    # }
+            # Print the JSON data for debugging
+            print("JSON Data HRV:", data_hrv)
+            print("JSON Data sleep:", data_sleep)
 
+            BASE_URL = "https://runalyze.com"
 
-    # Print the JSON data for debugging
-    # print("JSON Data HRV:", data_hrv)
-    # print("JSON Data sleep:", data_sleep)
+            # URL for the PUT request
+            sleep_url = BASE_URL + "/api/v1/metrics/sleep"
+            hrv_url = BASE_URL + "/api/v1/metrics/hrv"
 
-
-########################################################################
-# Make the Put request
-########################################################################
-
-BASE_URL = "https://runalyze.com"
-
-# URL for the PUT request
-sleep_url = BASE_URL + "/api/v1/metrics/sleep"
-hrv_url = BASE_URL + "/api/v1/metrics/hrv"
+            # Token for authentication
+            # Here you'll need to create a config.json file with your private token
+            # Load token from config file
+            token = get_token()
 
 
-# Token for authentication
-# Here you'll need to create a config.json file with your private token
-# Load token from config file
-with open("config.json", "r") as config_file:
-    config_data = json.load(config_file)
-    token = config_data["token"]
+            # Headers containing the token
+            headers = {
+                "token": token,
+                "Content-Type": "application/json",
+            }
+
+            # Make the POST request for sleep
+            response_sleep = requests.post(sleep_url, json=data_sleep, headers=headers)
+            # Check if the request was successful
+            print("Sleep Response Status Code:", response_sleep.status_code)
+            print("Sleep Response Body:", response_sleep.text)
+
+            # Make the POST request for HRV
+            response_hrv = requests.post(hrv_url, json=data_hrv, headers=headers)
+            # Check if the request was successful
+            print("HRV Response Status Code:", response_hrv.status_code)
+            print("HRV Response Body:", response_hrv.text)
 
 
-# Headers containing the token
-headers = {
-    "token": token,
-    "Content-Type": "application/json",
-}
+def get_token():
+    try:
+        with open("config.json", "r") as config_file:
+            config_data = json.load(config_file)
+            return config_data["token"]
+    except FileNotFoundError:
+        token_window = tk.Toplevel(root)
+        token_window.title("Enter Token")
 
-# Make the POST request for sleep
-response_sleep = requests.post(sleep_url, json=data_sleep, headers=headers)
-# Check if the request was successful
-print("Sleep Response Status Code:", response_sleep.status_code)
-print("Sleep Response Body:", response_sleep.text)
+        token_label = tk.Label(token_window, text="Enter your private token:")
+        token_label.pack()
 
-# Make the POST request for HRV
-response_hrv = requests.post(hrv_url, json=data_hrv, headers=headers)
-# Check if the request was successful
-print("HRV Response Status Code:", response_hrv.status_code)
-print("HRV Response Body:", response_hrv.text)
+        token_entry = tk.Entry(token_window)
+        token_entry.pack()
+
+        def save_token():
+            token = token_entry.get()
+            with open("config.json", "w") as config_file:
+                json.dump({"token": token}, config_file)
+            token_window.destroy()
+
+        submit_button = tk.Button(token_window, text="Submit", command=save_token)
+        submit_button.pack()
+
+        token_window.wait_window(token_window)
+
+        with open("config.json", "r") as config_file:
+            config_data = json.load(config_file)
+            return config_data["token"]
+
+
+root = tk.Tk()
+root.title("JSON File Reader")
+
+
+label = tk.Label(root, text="Click here to select your JSON file")
+label.pack()
+
+root.bind("<Button-1>", lambda e: open_file())  # Bind left-click to open file dialog
+
+root.mainloop()
+
+
+
+
+
 
